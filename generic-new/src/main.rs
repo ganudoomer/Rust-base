07489b7946fff7442e02772364b23e66d7f8d47f
sreeganesh <ganuiscool@gmail.com>
2022-07-30 20:54:48 +0530

    Generics

--- generic-new/src/main.rs
+++ generic-new/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
+    let chars = vec!["y", "d", "d", "b"];
+    let largest = get_largest(chars);
+    println!("{}", largest);
+    // Structs also work with generics
+}
+
+fn get_largest<T: PartialOrd + Copy>(number_list: Vec<T>) -> T {
+    let mut larges = number_list[0];
+    for number in number_list {
+        if number > larges {
+            larges = number;
+        }
+    }
+    larges
 }
