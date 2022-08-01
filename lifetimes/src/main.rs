baecd798f2cebc2ff819f23699f61d308c96451f
sreeganesh <ganuiscool@gmail.com>
2022-08-01 20:05:52 +0530

    Lifetimes

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 fn main() {
     // Borrow checker checks for ref in compile time
 
@@ -9,11 +11,37 @@ fn main() {
     // }
     // println!("Hello, world! {}", r);
 
-    // let string1 = String::from("Hey there");
-    // let string2 = String::from("Hey three");
+    let string1 = String::from("Hey there");
+    let string2 = String::from("Hey three afsdf");
 
-    // let result = longest(&string1, &string2);
+    let result = longest(&string1, &string2);
+    println!("{}", result);
+
+    let res = longest_with_an_announcement(&string1, &string2, "Hey");
+    println!("{}", res);
+
+    // This also does not work because
+    // We use a ref of variable that the lifetime that has ended
+
+    // let string4 = String::from("Hey three afsdf");
+    // let result;
+    // {
+    //     let string3 = String::from("Hey there");
+    //     result = longest(&string3, &string4);
+    // }
     // println!("{}", result);
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
 
 // we don't which value is going to live
@@ -25,3 +53,11 @@ fn main() {
 //         y
 //     }
 // }
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
