3844a32c1351b11a9f6779cf252c807c80cd932d
ganudoomer <ganuiscool@gmail.com>
2023-01-02 08:26:52 +0100

    Learn concurrency

--- concurrency-message-passing/src/main.rs
+++ concurrency-message-passing/src/main.rs
@@ -1,3 +1,30 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = mpsc::channel();
+
+    let tx2 = tx.clone();
+    thread::spawn(move || {
+        let values = vec!["someone", "something", "this"];
+
+        for val in values {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let values = vec!["someone", "something", "this"];
+
+        for val in values {
+            tx2.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
